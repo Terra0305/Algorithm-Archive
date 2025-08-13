3e6c7e70b94a0612f21c9499be317faffa9fb4b6
Laon <jpsm0305@naver.com>
2025-08-13 23:28:19 +0900

    Solve : Baek1302 베스트셀러

---
+++ src/Baek1302.java
@@ -0,0 +1,79 @@
+import java.util.*;
+public class Baek1302 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc= new Scanner(System.in);
+        
+       
+        int a = Integer.parseInt(sc.nextLine());
+
+        Map<String,Integer>BookMap = new HashMap<>();
+
+
+        for(int i=0;i<a;i++)
+        {
+            String Bookname = sc.nextLine();
+
+            int currentcount =BookMap.getOrDefault(Bookname, 0);
+
+            BookMap.put(Bookname, currentcount+1);
+        }
+
+        Set<String>Keyset = BookMap.keySet();
+
+        List<String>KeyList = new ArrayList<>(Keyset);
+
+        String Bestseller = new String();
+        int max = 0;
+
+        for(int i=0;i<KeyList.size();i++)
+        {
+            String currentbook = KeyList.get(i);
+            int currentcount = BookMap.get(currentbook);
+
+            if(currentcount>max){
+                 max=currentcount;
+                Bestseller=currentbook;
+            }else if(currentcount == max){
+                if(currentbook.compareTo(Bestseller)<0){
+                    Bestseller = currentbook;
+                }
+            }
+        }
+            System.out.println(Bestseller);
+
+    }
+}
+/*김형택은 탑문고의 직원이다. 김형택은 계산대에서 계산을 하는 직원이다. 김형택은 그날 근무가 끝난 후에, 오늘 판매한 책의 제목을 보면서 가장 많이 팔린 책의 제목을 칠판에 써놓는 일도 같이 하고 있다.
+
+오늘 하루 동안 팔린 책의 제목이 입력으로 들어왔을 때, 가장 많이 팔린 책의 제목을 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 오늘 하루 동안 팔린 책의 개수 N이 주어진다. 이 값은 1,000보다 작거나 같은 자연수이다. 둘째부터 N개의 줄에 책의 제목이 입력으로 들어온다. 책의 제목의 길이는 50보다 작거나 같고, 알파벳 소문자로만 이루어져 있다.
+
+출력
+첫째 줄에 가장 많이 팔린 책의 제목을 출력한다. 만약 가장 많이 팔린 책이 여러 개일 경우에는 사전 순으로 가장 앞서는 제목을 출력한다.
+
+예제 입력 1 
+5
+top
+top
+top
+top
+kimtop
+예제 출력 1 
+top
+예제 입력 2 
+9
+table
+chair
+table
+table
+lamp
+door
+lamp
+table
+chair
+예제 출력 2 
+table
+*/
