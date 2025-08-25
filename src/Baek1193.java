e4f99056237d5304589c2a22af69e2b90c29c1f4
Laon <jpsm0305@naver.com>
2025-08-25 18:54:08 +0900

    Solve : 백준1193 분수찾기

---
+++ src/Baek1193.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Baek1193 {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        
+        int sum=0;
+
+        int bunmo =0;
+        int bunja =0;
+
+
+        for(int i=1;i<=N;i++){
+            sum+=i;
+
+            if(sum>=N){
+                if(i%2==0){
+                    bunmo = sum-N+1;
+                    bunja = i-(sum-N);
+                    break;
+                }else{
+                    bunja = sum-N+1;
+                    bunmo = i-(sum-N);
+                    break;
+                }
+            }
+        }        
+        sb.append(bunja).append("/").append(bunmo);
+        System.out.println(sb);
+    }
+}
