f89b0ccd6002e3c73684f7ec3a29eaf6409ee2d2
Laon <jpsm0305@naver.com>
2025-08-28 16:31:49 +0900

    Solve : 백준10974

---
+++ src/Baek10974.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Baek10974 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        
+
+        System.out.println(sb);
+    }
+}
---
+++ src/Baek2606.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Baek2606 {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        // 여기에 실제 문제 풀이 코드 작성
+        int N = Inte
+
+        System.out.println(sb);
+    }
+}
