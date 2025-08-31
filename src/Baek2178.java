fc8862bf8d99cd4df62491f7d182c4f588564baa
Laon <jpsm0305@naver.com>
2025-08-31 17:47:51 +0900

    Solve : 백준2178 미로

--- src/Baek2178.java
+++ src/Baek2178.java
@@ -18,39 +18,34 @@ public class Baek2178 {
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
             for (int j = 0; j < M; j++) {
-                graph[i][j] = line.charAt(j) - '0'; // N x M 미로 정보 넣기.
+                graph[i][j] = line.charAt(j) - '0';
             }
         }
         visited[0][0] = true;
         queue.add(new int[] { 0, 0 });
 
         while (!queue.isEmpty()) {
-            int[] currentxy = queue.poll();
+            int[] currentXY = queue.poll();
+            int x = currentXY[0];// x,y좌표를 뽑아냄.
+            int y = currentXY[1];
 
-            int x = currentxy[0];
-            int y = currentxy[1];
-
-            int[] dx = { 0, 0, 1, -1 };
+            int[] dx = { 0, 0, -1, 1 };
             int[] dy = { 1, -1, 0, 0 };
 
             for (int i = 0; i < 4; i++) {
-                int nextX = x + dx[i]; // 상하 좌우 돌려보기.
+                int nextX = x + dx[i];
                 int nextY = y + dy[i];
 
                 if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M)
                     continue;
+                
+                if(!visited[nextX][nextY] && graph[nextX][nextY] ==1){
+                    visited[nextX][nextY] =true;
+                    queue.add(new int[] {nextX,nextY});
 
-                if (visited[nextX][nextY] || graph[nextX][nextY] == 0)
-                    continue;
-
-                if (graph[nextX][nextY] == 1) {
-                    graph[nextX][nextY] = graph[x][y] + 1;
-                    queue.add(new int[] { nextX, nextY });
-
+                    graph[nextX][nextY] = graph[x][y]+1;
                 }
-
             }
-
         }
 
         System.out.println(graph[N - 1][M - 1]);
