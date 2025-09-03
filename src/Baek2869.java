b9edeacb9dec30f8f766b5fd5e26e52c5ed49ce2
Laon <jpsm0305@naver.com>
2025-09-03 14:07:23 +0900

    Solve : 백준2869 달팽이는 올라가고싶다

--- src/Baek2869.java
+++ src/Baek2869.java
@@ -6,15 +6,20 @@ public class Baek2869 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
-        int A = Integer.parseInt(st.nextToken()); //낮에 올라가는 정도
-        int B = Integer.parseInt(st.nextToken()); //미끄러지는 정도
-        int V = Integer.parseInt(st.nextToken()); //정상 높이
+        int A = Integer.parseInt(st.nextToken()); // 낮
+        int B = Integer.parseInt(st.nextToken()); // 밤
+        int V = Integer.parseInt(st.nextToken()); // 높이
 
-        sb.append(V/(A-B)+1);
+        // 1. (V - A)를 (A - B)로 나눈 몫이 '마지막 날 전까지' 걸리는 기본 날짜
+        int days = (V - A) / (A - B);
         
-
-        System.out.println(sb);
+        // 2. 만약 나머지가 있다면, 하루가 더 필요함
+        if ((V - A) % (A - B) != 0) {
+            days++;
+        }
+        
+        // 3. 마지막 날 하루를 더해준다
+        System.out.println(days + 1);
     }
 }
