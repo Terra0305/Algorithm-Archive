28340851c440ae34e206d504bf974b7bf7a2f4cd
Laon <jpsm0305@naver.com>
2025-09-04 10:09:15 +0900

    BFS Finish 2차원 BFS 마친 후 기본BFS까지 복습

---
+++ src/Baek2606review.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Baek2606review {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer>[] graph = new ArrayList[N + 1];
+        boolean[] visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int c1 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+
+            graph[c1].add(c2);
+            graph[c2].add(c1);
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        visited[1] = true;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for (int i = 0; i < graph[current].size(); i++) {
+                int neighbor = graph[current].get(i);
+
+                if (!visited[neighbor]) {
+                    queue.add(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+        int count = 0;
+
+        for (int i = 0; i < visited.length; i++) {
+            if (visited[i] == true) {
+                count++;
+            }
+        }
+        sb.append(count-1);
+        System.out.println(sb);
+    }
+}
