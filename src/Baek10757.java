1129db2cb054dd7b4e3fa7995216ef7a61a2dc44
Laon <jpsm0305@naver.com>
2025-09-26 16:35:19 +0900

    Solve : 백준10757 큰수

--- src/Baek10757.java
+++ src/Baek10757.java
@@ -1,3 +1,22 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
 public class Baek10757 {
-    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String stringA = st.nextToken();
+        String stringB = st.nextToken();
+
+        BigInteger A = new BigInteger(stringA);
+        BigInteger B = new BigInteger(stringB);
+
+        BigInteger sum = A.add(B);
+
+        System.out.println(sum);
+
+    }
 }
