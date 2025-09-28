bfbee4a60dbc2d3bc4de0e3ba3b6126c1dad71d2
Laon <jpsm0305@naver.com>
2025-09-28 22:51:07 +0900

    Rename Baek1057

--- src/Baek1057.java
+++ src/Baek1057.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Baek1966 {
+public class Baek1057 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
