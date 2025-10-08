c3aa07724ba0428a33c49b4d83cfa56a478f60a3
Park Sungmin <jpsm0305@naver.com>
2025-10-08 23:43:35 +0900

    Solve : 백준 2747 피보나치 DP

---
+++ src/Baek2747.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Baek2747 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int [] dp = new int[46];
+        dp[0] = 0;
+        dp[1] =1;
+
+        for(int i=2;i<dp.length;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        int n = Integer.parseInt(br.readLine());
+
+        sb.append(dp[n]);
+        System.out.println(sb);
+    }
+}
