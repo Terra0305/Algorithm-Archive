0ca40e46be3307cf4d83a2dd1be5d6d528e15a50
Laon <jpsm0305@naver.com>
2025-10-10 17:28:57 +0900

    Solve : 백준27331

---
+++ src/Baek27331.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Baek27331 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String a = br.readLine();
+        String b = br.readLine();
+
+        sb.append(a+b);
+        System.out.println(sb);
+    }
+}
