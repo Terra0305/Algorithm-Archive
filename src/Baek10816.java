3f37e90a80f51076b9b83fe96b94fd2deb9e1047
PARK SUNGMIN <jpsm0305@naver.com>
2025-10-13 10:40:17 +0900

    Resolve : 백준10816 해시맵

--- src/Baek10816.java
+++ src/Baek10816.java
@@ -1,47 +1,31 @@
+import java.io.*;
 import java.util.*;
+
 public class Baek10816 {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc= new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
         
-        int N = sc.nextInt();
-
-        Map<Integer,Integer> CardNum= new HashMap<>();
-
-        for(int i=0;i<N;i++)
-        {
-            int card = sc.nextInt();
-            CardNum.put(card, CardNum.getOrDefault(card, 0)+1);
-        }
-
-        int M = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        for(int i=0;i<M;i++)
-        {
-             int targetNum = sc.nextInt();
-             sb.append(CardNum.getOrDefault(targetNum,0)).append(" ");
-        }
-        
-        System.out.println(sb.toString().trim());
+        int n = Integer.parseInt(br.readLine());
 
-        
+        HashMap<Integer,Integer> Target = new HashMap<>();
+
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            int a = Integer.parseInt(st.nextToken());
+            Target.put(a,Target.getOrDefault(a,0)+1); //해시맵 입력 + 숫자 나온 횟수 등록
+        }
+
+        int j = Integer.parseInt(br.readLine());
+
+        st= new StringTokenizer(br.readLine());
+
+        for(int k=0;k<j;k++){
+            int a = Integer.parseInt(st.nextToken());
+            sb.append(Target.getOrDefault(a, 0)).append(" ");
+        }
+
+        System.out.println(sb);
     }
 }
-/*숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다. 정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 몇 개 가지고 있는지 구하는 프로그램을 작성하시오.
-
-입력
-첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 둘째 줄에는 숫자 카드에 적혀있는 정수가 주어진다. 숫자 카드에 적혀있는 수는 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
-
-셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 넷째 줄에는 상근이가 몇 개 가지고 있는 숫자 카드인지 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다. 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
-
-출력
-첫째 줄에 입력으로 주어진 M개의 수에 대해서, 각 수가 적힌 숫자 카드를 상근이가 몇 개 가지고 있는지를 공백으로 구분해 출력한다.
-
-예제 입력 1 
-10
-6 3 2 10 10 10 -10 -10 7 3
-8
-10 9 -5 2 3 4 5 -10
-예제 출력 1 
-3 0 0 1 2 0 0 2*/
---
+++ src/CodetreeHashmap.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class CodetreeHashmap {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        
+
+        System.out.println(sb);
+    }
+}
