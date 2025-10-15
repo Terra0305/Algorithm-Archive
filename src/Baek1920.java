4b3870030febd3793940e9c284d8b3a37f0f4fd1
PARK SUNGMIN <jpsm0305@naver.com>
2025-10-15 12:59:38 +0900

    Resolve (투포인터 복습

--- src/Baek1920.java
+++ src/Baek1920.java
@@ -1,53 +1,49 @@
+import java.io.*;
 import java.util.*;
 
 public class Baek1920 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        int N = Integer.parseInt(sc.nextLine());
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        Set<Integer> Nset = new HashSet<>();
 
-        for (int i = 0; i < N; i++) {
-            Nset.add(sc.nextInt());// 값 모두넣기
+        int n = Integer.parseInt(br.readLine());
+        int[] narr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            narr[i] = Integer.parseInt(st.nextToken());
         }
+                Arrays.sort(narr);
 
-        int M = sc.nextInt();
-        for(int i=0;i<M;i++){
-            int target = sc.nextInt();
-            if(Nset.contains(target)){
+        int N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            int target=Integer.parseInt(st.nextToken());
+
+            if(BinarySearch(narr, target)){
                 sb.append(1+"\n");
-            }
-            else
+            }else{
                 sb.append(0+"\n");
+            }
         }
-        System.out.println(sb.toString());
-   
+        System.out.println(sb);
     }
 
+    public static boolean BinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid]==target){
+                return true;
+            }else if(arr[mid]<target){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return false;
+    }
 }
-
-/*
- * N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
- * 
- * 입력
- * 첫째 줄에 자연수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 주어진다.
- * 다음 줄에는 M(1 ≤ M ≤ 100,000)이 주어진다. 다음 줄에는 M개의 수들이 주어지는데, 이 수들이 A안에 존재하는지 알아내면
- * 된다. 모든 정수의 범위는 -231 보다 크거나 같고 231보다 작다.
- * 
- * 출력
- * M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
- * 
- * 예제 입력 1
- * 5
- * 4 1 5 2 3
- * 5
- * 1 3 7 9 5
- * 예제 출력 1
- * 1
- * 1
- * 0
- * 0
- * 1
- */
--- src/Baek3273.java
+++ src/Baek3273.java
@@ -8,37 +8,45 @@ public class Baek3273 {
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
         int target = Integer.parseInt(br.readLine());
+
         Arrays.sort(arr);
-        sb.append(Twopointer(arr, target));
+        sb.append(twopointer(arr, target));
+
         System.out.println(sb);
     }
 
-    public static int Twopointer(int[] arr, int target) {
+    public static int twopointer(int[] a, int target) {
         int count = 0;
+
         int start = 0;
-        int end = arr.length - 1;
+        int end = a.length - 1;
 
         while (start < end) {
-            int sum = arr[start] + arr[end];
+            int sum = a[start] + a[end];
 
-            if (sum < target)
+            if(sum<target){
                 start++;
-            else if (sum > target)
+            }else if(sum>target){
                 end--;
-            else {
-                count++;
-                start++;
             }
-        }
-        return count;
 
+            else{
+                start++;
+                end--;
+                count++;
+            }
+
+        }
+
+        return count;
     }
 }
