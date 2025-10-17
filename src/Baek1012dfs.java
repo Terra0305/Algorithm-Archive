1adffb24a8fad4d9a4668b62d2007aa308d45517
PARK SUNGMIN <jpsm0305@naver.com>
2025-10-18 08:48:01 +0900

    Resolve 유기농배추

--- src/Baek1012dfs.java
+++ src/Baek1012dfs.java
@@ -3,13 +3,11 @@ import java.util.*;
 
 public class Baek1012dfs {
     static int[][] map;
-    static int N;
-    static int M;
-    static int K;
-
     static boolean[][] visited;
-
-    static int[] dx = { 0, 0, 1, -1 };
+    static int M;
+    static int N;
+    static int K;
+    static int[] dx = { 0, 0, -1, 1 };
     static int[] dy = { 1, -1, 0, 0 };
 
     public static void main(String[] args) throws IOException {
@@ -22,45 +20,46 @@ public class Baek1012dfs {
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
 
-            map = new int[N][M];
-            visited = new boolean[N][M];
+            map = new int[M][N];
+            visited = new boolean[M][N];
 
             for (int j = 0; j < K; j++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
+
                 map[x][y] = 1;
             }
-            int wormcount = 0;
-            for (int x = 0; x < N; x++) {
-                for (int y = 0; y < M; y++) {
-                    if (map[x][y] == 1 && !visited[x][y]) {
-                        wormcount++;
-                        dfs(x, y);
+
+            int bugcount = 0;
+            for (int q = 0; q < M; q++) {
+                for (int w = 0; w < N; w++) {
+                    if (map[q][w] == 1 && !visited[q][w]) {
+                        dfs(q, w);
+                        bugcount++;
                     }
                 }
             }
-            sb.append(wormcount).append("\n");
-
+            sb.append(bugcount).append("\n");
         }
-        System.out.println(sb);
 
+        System.out.println(sb);
     }
 
     public static void dfs(int x, int y) {
         visited[x][y] = true;
-        for (int i = 0; i < 4; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
 
-            if (nextX >= 0 && nextY >= 0 && nextX < N && nextY < M && !visited[nextX][nextY]
-                    && map[nextX][nextY] == 1) {
-                dfs(nextX, nextY);
-            }
+        for (int i = 0; i < 4; i++) {
+            int nextx = x+dx[i];
+            int nexty = y+dy[i];
+
+            if (nextx >= 0 && nexty >= 0 && nextx < M && nexty < N && !visited[nextx][nexty] && map[nextx][nexty] == 1)
+                dfs(nextx, nexty);
         }
+
     }
 }
