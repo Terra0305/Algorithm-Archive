ca75c50ca493aa0476191fcdf5fe82dc245297a9
PARK SUNGMIN <jpsm0305@naver.com>
2025-12-20 21:15:20 +0900

    12/20 최빈 알파벳 풀이

--- 딩코딩코Python/최빈알파벳.py
+++ 딩코딩코Python/최빈알파벳.py
@@ -1,7 +1,22 @@
 def find_max_occurred_alphabet(string):
-    # 이 부분을 채워보세요!
-    return "a"
-
+    alphabet_array=[0]*26
+    max=0
+    
+    for char in string:
+        if not char.isalpha():
+            continue
+        alphabet_index= ord(char)-ord('a') #알파벳을 숫자로 변환시켜줌 
+        alphabet_array[alphabet_index]+=1 #등장한 알파벳의 숫자 +1
+    
+    for index in range(len(alphabet_array)): #알파벳의 빈도를 담은 배열 순회      
+        now_index = alphabet_array[index]
+        
+        if max < now_index:
+            max=now_index #알파벳의 최빈값
+            
+        
+    return chr(alphabet_array.index(max) + ord('a'))
+                
 
 result = find_max_occurred_alphabet
 print("정답 = i 현재 풀이 값 =", result("hello my name is dingcodingco"))
