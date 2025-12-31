8c6aab968854735770af32c7e415adc39ab63104
PARK SUNGMIN <jpsm0305@naver.com>
2025-12-31 22:24:59 +0900

    일단저장

---
+++ 딩코딩코Python/프로그래머스_기능개발.py
@@ -0,0 +1,10 @@
+def solution(progresses, speeds):
+    answer = []
+    current_progresses =0 #현재 완료되어야 하는 작업 인덱스
+    success = 1 #완료된 숫자 
+    while True:
+        for i in range(len(progresses)-1):
+         answer.append   
+    return answer
+
+print(solution([93,30,55],[1,30,5]))
