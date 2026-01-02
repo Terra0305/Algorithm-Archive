715c39d29e1c875c8845f572968fb3411d558651
PARK SUNGMIN <jpsm0305@naver.com>
2026-01-02 19:11:37 +0900

    프로그래머스_고득점킷_해시_완주하지못한 선수 풀이

---
+++ 딩코딩코Python/프로그래머스_고득점킷_해시_폰켓몬.py
@@ -0,0 +1,16 @@
+def solution(nums):
+    dict={} #포켓몬을 담을 딕셔너리
+    can_get_num = len(nums)/2
+    get_count=0
+    
+    
+    
+    
+    answer = 0
+    return answer
+
+print(solution([3,1,2,3]))
+
+#1. 들어온 배열 원소의 갯수 세기. N/2마리를 가져간다 했으니 최우선적으로 구해야함.
+#2. for문으로 순회하며 딕셔너리에 포켓몬 이름과 나온 횟수를 구한다.
+# 가져갈수있는 마릿수가 찰때까지 나온횟수가 가장 낮은 포켓몬들을 pick. 종이 다르면 count+=1
