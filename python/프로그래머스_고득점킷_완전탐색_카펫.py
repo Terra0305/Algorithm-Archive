8f9d88eeaffa0e4dda878dbaecb274033e8dca2b
PARK SUNGMIN <jpsm0305@naver.com>
2026-01-09 17:00:46 +0900

    .

--- python/프로그래머스_고득점킷_완전탐색_카펫.py
+++ python/프로그래머스_고득점킷_완전탐색_카펫.py
@@ -1,15 +1,12 @@
 def solution(brown, yellow):
     answer = []
-    total = brown+yellow
-    
+    total = brown + yellow
     for sero in range(3,int(total**0.5)+1):
-        if total%sero==0:
+        if total%sero ==0:
             garo = total//sero
             
             if (garo-2)*(sero-2) ==yellow:
-                    answer= [garo,sero]
-        
+                answer = [ garo,sero]
     
     return answer
-
 print(solution(10,2))
