9c81578c3803e4941c4db986f6cb4be782a00b51
PARK SUNGMIN <jpsm0305@naver.com>
2026-01-21 12:20:08 +0900

    네트워크 복습

---
+++ python/프로그래머스_네트워크 복습.py
@@ -0,0 +1,16 @@
+from collections import deque
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    for i in range(n):
+        if not visited[i]:
+            queue=deque([i])
+            visited[i]= True
+            while queue:
+                curr = queue.popleft()
+                for neighbor,connected in enumerate(computers[curr]):
+                    if connected==1 and not visited[neighbor]:
+                        visited[neighbor]= True
+                        queue.append(neighbor)  
+            answer+=1
+    return answer
